b8100772c4426c09a8d91e627f9b3eebebfc0034
Piotr Zawadzki <115027061+squw0@users.noreply.github.com>
2024-10-30 00:32:58 +0100

    Delete Assets/Scripts directory
    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- Assets/Scripts/Collectible.cs
+++
@@ -1,6 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-public abstract class Collectible : MonoBehaviour, ICollectible{
-    public abstract void Collect();
-}
